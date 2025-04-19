e94793d46e4083387d20911296f4b22b82e0dbcf
Easu115597 <92202717+Easu115597@users.noreply.github.com>
2025-04-19 11:31:01 +0800

    Update run.py

--- run.py
+++ run.py
@@ -25,6 +25,8 @@ except ImportError:
         logger.addHandler(handler)
         return logger
 
+
+
 # 創建記錄器
 logger = setup_logger("main")
 
@@ -53,12 +55,12 @@ def parse_arguments():
     # 馬丁策略專用參數組
     martingale_group = parser.add_argument_group('馬丁策略參數')
     martingale_group.add_argument('--martingale', action='store_true', help='啟用馬丁策略')
-    martingale_group.add_argument('--martingale-symbol',dest='martingale_symbol', type=str, default='BTC_USDC', help='馬丁策略交易對')
+    martingale_group.add_argument('--martingale-symbol',dest='martingale_symbol', type=str, default='SOL_USDC', help='馬丁策略交易對')
     martingale_group.add_argument('--total-capital', dest='total_capital', type=float, default=100, help='馬丁策略總投入 USDT')
     martingale_group.add_argument('--price_step_down',dest='price_step_down', type=float, default=0.008, help='加倉下跌百分比')
     parser.add_argument('--take_profit_pct', type=float, default=0.012, help='止盈百分比')
     parser.add_argument('--stop_loss_pct', type=float, default=-0.33, help='停損百分比')
-    martingale_group.add_argument('--max-layers', dest='max_layers', type=int, required=True, help='最大加倉層數')
+    martingale_group.add_argument('--max-layers', dest='max_layers', type=int, default=5, help='最大加倉層數')
     parser.add_argument('--multiplier', type=float, default=1.3, help='馬丁加碼倍率')
     parser.add_argument('--use_market_order', action='store_true', help='是否使用市價初始下單')
     parser.add_argument('--entry-price', type=float, help='指定首次入場目標價格')
@@ -100,6 +102,10 @@ def main():
     elif args.martingale:
         try:
             from martingale_mode_long import MartingaleLongTrader
+
+            if args.martingale_symbol:
+                args.martingale_symbol = args.martingale_symbol.replace('_', '-')
+
             trader = MartingaleLongTrader(
                 api_key=api_key,
                 secret_key=secret_key,
